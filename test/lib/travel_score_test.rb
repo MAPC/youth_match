80a1d4d554233bfa3fd4601cd19ae92e180564d0
Matt Cloyd <cloydster@gmail.com>
2016-03-01 17:19:08 -0500

    Add Travel Score
    Why:
    
    Need to calculate a time-based score in order to create a full composite
    match score.
    
    This change addresses the need by:
    
    * Test-driving the TravelTime score class

--- test/lib/travel_score_test.rb
+++ test/lib/travel_score_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class TravelScoreTest < Minitest::Test
 
-  def teardown
-    assert applicant.verify
-    assert position.verify
-  end
-
   def applicant
     @_applicant ||= Minitest::Mock.new
   end
@@ -19,37 +14,68 @@ class TravelScoreTest < Minitest::Test
     @_score ||= TravelScore.new(applicant: applicant, position: position)
   end
 
-  def test_score
-    skip "Let's figure out a test strategy here"
-    assert score.score
+  def test_travel_score_when_time_negative
+    applicant.expect :prefers_nearby?, false
+    applicant.expect :mode, 1
+    applicant.expect :grid_id, 1
+    position.expect  :grid_id, 1
+    TravelTime.stub :find_by, TravelTime.new(time: -1.seconds) do
+      score = TravelScore.new(applicant: applicant, position: position)
+      assert_raises(StandardError) { score.score }
+    end
   end
 
-  def test_travel_score
-    skip "Let's figure out a test strategy here"
-    applicant.expect(:travel_time_to, 10.minutes)
-    applicant.expect(:prefers_nearby?, :false)
-    assert_in_delta score.score, 3, 1
+  def test_travel_score_when_doesnt_care
+    dont_care_scores.each do |hash|
+      applicant.expect :prefers_nearby?, false
+      applicant.expect :mode, 1
+      applicant.expect :grid_id, 1
+      position.expect  :grid_id, 1
 
-    applicant.expect(:prefers_nearby?, :true)
-    assert_in_delta score.score, 0, 1
+      TravelTime.stub :find_by, TravelTime.new(time: hash[:time].minutes) do
+        expected = hash[:score]
+        actual = TravelScore.new(applicant: applicant, position: position)
+        assert_in_delta expected, actual.score, 0.5
+      end
+      assert applicant.verify
+    end
+
   end
 
-=begin
+  def test_travel_score_when_cares
+    care_scores.each do |hash|
+      applicant.expect :prefers_nearby?, true
+      applicant.expect :mode, 1
+      applicant.expect :grid_id, 1
+      position.expect  :grid_id, 1
 
-SCORE REFERENCE POINTS
+      TravelTime.stub :find_by, TravelTime.new(time: hash[:time].minutes) do
+        expected = hash[:score]
+        actual = TravelScore.new(applicant: applicant, position: position)
+        assert_in_delta expected, actual.score, hash[:delta]
+      end
+      assert applicant.verify
+    end
+  end
 
-    Don't care      Care
+  private
 
- 5  Job is in same grid cell
+    def care_scores
+      [{score:  3, time: 5,  delta: 0.75},
+       {score:  0, time: 10, delta: 0.5},
+       {score: -3, time: 20, delta: 0.5},
+       {score: -5, time: 30, delta: 0.5},
+       {score: -5, time: 40, delta: 0.5},
+       {score: -5, time: 99, delta: 0.5}]
+    end
 
- 3  10 minutes       5 minutes
-
- 0  20 minutes      10 minutes
-
--3  30 minutes      20 minutes
-
--5  > 40 minutes
-
-=end
+    def dont_care_scores
+      [{score:  3, time: 10},
+       {score:  0, time: 20},
+       {score: -3, time: 30},
+       {score: -5, time: 40},
+       {score: -5, time: 45},
+       {score: -5, time: 99}]
+    end
 
 end
