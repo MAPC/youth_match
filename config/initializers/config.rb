3d69717bcb8abcf761d2abdd8f10e3d0e78e4042
Matt Cloyd <cloydster@gmail.com>
2016-04-06 13:19:42 -0400

    Break out coercion into its own method

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -8,6 +8,16 @@ module Initializers
 
     def self.load
       $config = Hash.to_ostructs(config_from_yaml)
+      coerce_config_types
+    end
+
+    private
+
+    # If a DATABASE_URL is given for an environment, turn it into a string.
+    # If not given, turn it into a Hash.
+    # Yes, this is in large part reversing the ostructing above.
+    # We live to optimize another day.
+    def self.coerce_config_types
       %i( development test production ).each do |env|
         params = $config.send(env)
         if params.try(:url)
@@ -16,10 +26,7 @@ module Initializers
           $config.send("#{env}=", params.to_h)
         end
       end
-      puts "====> #{$config.inspect}"
     end
-
-    private
 
     def self.config_from_yaml
       config_file = File.read File.join(Dir.pwd, 'config', 'database.yml')
