6392c1f9950434bce776c9f3fa5ec2b0c45711dc
Matt Cloyd <cloydster@gmail.com>
2016-05-03 12:14:26 -0400

    Add environment to migration.
    Why:
    
    When Matt (@allthesignals) was migrating, it was throwing an error that
    Position wasn't available. This was because Run, which references
    Position, wasn't being loaded in the run.rb file -- most of the time, it
    just depends on the environment.
    
    This change addresses the need by:
    
    * Instead of explicitly requiring Position inside Run or in the
    migration, we just load in the whole environment!
    
    * Formatting the text to look a little better.

--- db/migrate/1457456437_make_statistics_non_null_with_default.rb
+++ db/migrate/1457456437_make_statistics_non_null_with_default.rb
@@ -1,15 +1,17 @@
 require 'enumerize'
-require './lib/models/run'
+require './environment'
 class MakeStatisticsNonNullWithDefault < ActiveRecord::Migration
 
   def up
-    Run.find_each {|run| run.update_attribute(:statistics, {}) if run.statistics.nil?}
-    change_column_null :runs, :statistics, false
+    Run.find_each do |run|
+      run.update_attribute(:statistics, {}) if run.statistics.nil?
+    end
+    change_column_null    :runs, :statistics, false
     change_column_default :runs, :statistics, {}
   end
 
   def down
-    change_column_null :runs, :statistics, true
+    change_column_null    :runs, :statistics, true
     change_column_default :runs, :statistics, nil
   end
 
