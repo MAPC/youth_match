865de96a773b513c148f195013f7958dc58f1f6d
Matt Cloyd <cloydster@gmail.com>
2016-05-03 12:13:52 -0400

    Update address accessor to use ICIMS address, and to be more verbose about what it's doing.

--- lib/jobs/position_geocoder.rb
+++ lib/jobs/position_geocoder.rb
@@ -3,23 +3,43 @@ require 'geocoder'
 class PositionGeocoder
 
   def perform
-    Position.where(location: nil).each do |position|
+    log_start
+    locationless_positions.each do |position|
       next if no_address(position)
-      results = Geocoder.search position.addresses.first.to_a.join(' ')
+      results = Geocoder.search position.address.to_a.join(' ')
       if results.any?
         position.location = point_from_results(results)
         position.save!
       end
       sleep 0.5
-      puts "\n\n"
     end
+    log_finish
   end
 
   private
 
+  def locationless_positions
+    Position.where(location: nil)
+  end
+
+  def log_start
+    @before = locationless_positions.count
+    $logger.debug "#{@before} positions have no location"
+  end
+
+  def log_finish
+    @after = locationless_positions.count
+    $logger.debug "Found a location for #{@before - @after} positions."
+    $logger.debug "#{@after} positions have no location"
+  end
+
   def point_from_results(results)
     r = results.first
     factory.point r.longitude, r.latitude
+  end
+
+  def factory
+    RGeo::Geographic.spherical_factory srid: 4326
   end
 
   def no_address(position)
